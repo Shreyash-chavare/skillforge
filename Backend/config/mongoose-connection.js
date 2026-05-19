e3d57d3ca4e02a6a0bbe8bcab3605827c3493426
Shreyash-chavare <shreyashchavare04@gmail.com>
2026-05-19 16:07:17 +0530

    fixedddy

--- Backend/config/mongoose-connection.js
+++ Backend/config/mongoose-connection.js
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
+const dbUri =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : `${process.env.DB_URI}/Skillforge`;
 
-const dbUri=process.env.NODE_ENV=='production'?process.env.MONGO_URI :process.env.DB_URI/Skillforge
-
-mongoose.connect(dbUri)
-.then(()=>{
-    console.log("Mongodb connected successfully")
-})
-.catch((err)=>{
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
-    console.error("Connection string used:", dbUri.replace(/\/\/[^:]+:[^@]+@/, '//***:***@'));
-})
-
-
+  });
