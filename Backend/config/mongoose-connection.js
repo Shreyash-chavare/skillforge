faa5e1494e7794229f865b1b787ab7c8fee56406
Shreyash-chavare <shreyashchavare04@gmail.com>
2026-05-19 16:02:48 +0530

    fixeddd

--- Backend/config/mongoose-connection.js
+++ Backend/config/mongoose-connection.js
@@ -1,16 +1,14 @@
 import mongoose from 'mongoose'
-import config from 'config'
-import dbgr from 'debug'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const dbg=dbgr("development:Mongoose")
 
-const dbUri=process.env.NODE_ENV=='production'?process.env.MONGO_URI :`${config.get("DB_URI")}/Skillforge`
+
+const dbUri=process.env.NODE_ENV=='production'?process.env.MONGO_URI :process.env.DB_URI/Skillforge
 
 mongoose.connect(dbUri)
 .then(()=>{
-    dbg("Mongodb connected successfully")
+    console.log("Mongodb connected successfully")
 })
 .catch((err)=>{
     console.error("❌ MongoDB connection error:", err.message);
