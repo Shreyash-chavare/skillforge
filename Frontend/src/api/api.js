295387d3fdd55ffd8aee152c81a13f33a9ce69fb
Shreyash-chavare <shreyashchavare04@gmail.com>
2026-05-19 16:13:24 +0530

    api..fixed

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api=axios.create({baseURL:"http://localhost:8000/api",withCredentials:true});
+const api=axios.create({baseURL:import.meta.env.VITE_API_URI,withCredentials:true});
 
 api.interceptors.request.use(
     (config)=>{
